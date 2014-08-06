f11280c4b6f5e0d96d22b8db2c431b0b9cd7949b
Joshua J. Campoverde <joshcampoverde@gmail.com>
2014-08-05 20:50:55 -0700

    Adding make_api_request and set_authorization_header helper methods to standardize API requests and reduce likelihood of error

--- digital_ocean.rb
+++ digital_ocean.rb
@@ -17,12 +17,8 @@ class DigitalOcean
   end
 
   def list_droplets
-    # puts "Authorization: Bearer #{self.token}"
     api_request_url = @api_base + "droplets"
-    api_request_payload = {}
-    api_request_headers = {}
-    api_request_headers[:Authorization] = "Bearer #{self.token}"
-    response = RestClient.get api_request_url, api_request_headers
+    response = make_api_request(api_request_url)
   end  
 
   def create_droplet(options={})
@@ -39,6 +35,32 @@ class DigitalOcean
 
   def token
     @token
+  end
+
+  # Standardize API calls using this helper method
+  #
+  def make_api_request(api_url, request_method=:GET, api_request_payload={}, api_request_headers={})
+    api_request_headers = set_authorization_header(api_request_headers)
+
+    case request_method
+    when :GET
+      response = RestClient.get api_url, api_request_headers
+    when :POST
+      response = RestClient.post api_url, api_request_payload, api_request_headers
+    when :PUT
+      puts "Error: PUT method not yet implemented for API calls."
+    when :DELETE
+      puts "Error: DELETE method not yet implemented for API calls."
+    else
+      puts "Error: Invalid HTTP method used for this request."
+    end
+  end
+
+  # Standardize the setting of the Authorization header with this
+  # helper method
+  #
+  def set_authorization_header(request_headers)
+    request_headers.merge({:Authorization => "Bearer #{self.token}"})
   end
 
 end
