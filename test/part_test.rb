2adffb7205fcecea887d125b5d88f24014542bb7
Josh Campoverde <joshcampoverde@gmail.com>
2014-08-30 16:31:13 -0700

    Using ENV variable for the token rather than a config.yml file.

--- test/part_test.rb
+++ test/part_test.rb
@@ -4,19 +4,19 @@ require 'yaml'
 
 require 'part'
 
-CONFIG = YAML.load_file("config.yml")
+@digital_ocean_token = ENV['DIGITAL_OCEAN_TOKEN']
 
 describe Part do
   describe "when initialized without a version number" do
     it "defaults to api v2" do
-      @part = Part.new(CONFIG['digital_ocean_token'])
+      @part = Part.new(@digital_ocean_token)
       @part.api_base.must_equal "https://api.digitalocean.com/v2/"
     end
   end
 
   describe "when initialized with a specific version number" do
     it "uses the correct api_base_url" do
-      @part = Part.new(CONFIG['digital_ocean_token'], 'v1')
+      @part = Part.new(@digital_ocean_token, 'v1')
       @part.api_base.wont_equal "https://api.digitalocean.com/v2/"
       @part.api_base.must_equal "https://api.digitalocean.com/v1/"
     end
